15d42f95a67b4d5f1295a5ccbed4f9c3b0805cd8
MarceloCaballeroo <marc-caballero@duocuc.cl>
2024-07-24 01:53:07 -0400

    Mejora visual

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.encryptText = function() {
-    const inputText = document.getElementById('inputText').value;
-    const outputText = document.getElementById('outputText');
+    const inputText = document.getElementById('inputText').value.trim();
 
-    // Validación para asegurarse de que no hay letras mayúsculas ni letras con tildes
+    if (inputText === "") {
+        resetRightSection();
+        return;
+    }
+
     if (/[A-ZÁÉÍÓÚáéíóú]/.test(inputText)) {
         document.querySelector('.right-section').innerHTML = `
             <p id="outputText" class="message">Solo letras minúsculas y sin acentos</p>
@@ -24,9 +27,13 @@ window.encryptText = function() {
 }
 
 window.decryptText = function() {
-    const inputText = document.getElementById('inputText').value;
+    const inputText = document.getElementById('inputText').value.trim();
 
-    // Validación para asegurarse de que no hay letras mayúsculas ni letras con tildes
+    if (inputText === "") {
+        resetRightSection();
+        return;
+    }
+
     if (/[A-ZÁÉÍÓÚáéíóú]/.test(inputText)) {
         document.querySelector('.right-section').innerHTML = `
             <p id="outputText" class="message">Solo letras minúsculas y sin acentos</p>
@@ -55,3 +62,30 @@ window.copyText = function() {
         alert('Error al copiar el texto');
     });
 }
+
+function resetRightSection() {
+    document.querySelector('.right-section').innerHTML = `
+        <img src="assets/Muñeco.svg" alt="Imagen ilustrativa" class="illustration">
+        <p id="outputText" class="message">Ningún mensaje fue encontrado</p>
+        <p class="description">Ingresa el texto que desees encriptar o desencriptar.</p>
+    `;
+}
+
+document.getElementById('toggleTheme').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    updateThemeIcon();
+});
+
+function updateThemeIcon() {
+    const icon = document.getElementById('themeIcon');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.src = 'assets/sun.svg';
+        icon.alt = 'Sun icon';
+    } else {
+        icon.src = 'assets/moon.svg';
+        icon.alt = 'Moon icon';
+    }
+}
+
+// Inicializa el icono al cargar la página
+document.addEventListener('DOMContentLoaded', updateThemeIcon);
